c0f5610959b666830646d909412823ddf0505bec
Bunnicz <kpodgorski367@gmail.com>
2022-05-12 21:30:44 +0200

    2nd commit Signed-off-by: Bunnicz <kpodgorski367@gmail.com>

--- main.py
+++ main.py
@@ -3,28 +3,26 @@ import settings as st
 import utils
 
 
-
-
 root = Tk()
 # Overide the settings of the window
 root.configure(bg="black")
-root.geometry(f'{st.WIDTH}x{st.HEIGHT}')
+root.geometry(f"{st.WIDTH}x{st.HEIGHT}")
 root.title("Minesweeper Game")
-root.resizable(False, False) # width, height
+root.resizable(False, False)  # width, height
 
 top_frame = Frame(
     root,
-    bg="red", # change later to black
+    bg="red",  # change later to black
     width=st.WIDTH,
-    height=utils.height_prct(25)
-    )
+    height=utils.height_prct(25),
+)
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='blue', # change later to black
+    bg="blue",  # change later to black
     width=utils.width_prct(25),
-    height=utils.height_prct(75)
+    height=utils.height_prct(75),
 )
 left_frame.place(x=0, y=utils.height_prct(25))
 
--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import settings
 
-def height_prct(percentage):
+
+def height_prct(percentage: int) -> int:
     return (settings.HEIGHT / 100) * percentage
-    
-def width_prct(percentage):
+
+
+def width_prct(percentage: int) -> int:
     return (settings.WIDTH / 100) * percentage
+
+
+if __name__ == "__main__":
+    print(height_prct(25))
+    print(width_prct(25))
